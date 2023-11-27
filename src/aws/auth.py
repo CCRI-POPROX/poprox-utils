3bcce127fbbd552bf13f6b14c8199aa43d40b4d9
Sumukh Nitundila <sumukh14@gmail.com>
2023-11-27 16:42:15 -0600

    Initial commit

---
+++ src/aws/auth.py
@@ -0,0 +1,22 @@
+from boto3 import Session
+
+
+class Auth:
+    __session = None
+
+    @classmethod
+    def get_boto3_session(cls, aws_access_key_id=None, aws_secret_access_key=None, aws_session_token=None,
+                          region_name=None) -> Session:
+        return cls.__get_boto3_session(aws_access_key_id, aws_secret_access_key, aws_session_token, region_name)
+
+    @classmethod
+    def __get_boto3_session(cls, aws_access_key_id=None, aws_secret_access_key=None, aws_session_token=None,
+                            region_name=None) -> Session:
+        if cls.__session is None:
+            cls.__session = Session(
+                aws_access_key_id=aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key,
+                aws_session_token=aws_session_token,
+                region_name=region_name
+            )
+        return cls.__session
